3b1d584163519c937a583476552ea1abe07c752d
John <johnleavell@gmail.com>
2021-01-21 22:59:15 -0600

    fixed heroku issue

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   resources :location_results
   resources :results
   resources :locations
-  root 'welcome#index'
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
